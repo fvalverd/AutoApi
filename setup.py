a97cb333ff7a99c57d5d5c2002f02a709e782409
Felipe Valverde <felipe.valverde.campos@gmail.com>
2015-04-27 19:47:33 -0300

    add: LICENSE. fix: travis coveralls

--- setup.py
+++ setup.py
@@ -6,14 +6,26 @@ version = '0.0.1'
 setup(
     name='AutoApi',
     version=version,
-    description="",
-    long_description="""""",
-    classifiers=[],
-    keywords='api rest auth mongodb3',
+    description="Automatic API REST",
+    long_description="""
+AutoApi was created to not wasting time to developing an API REST at the
+project start. Has an authentication system to provided secure data based on
+MongoDB 3.X.
+""",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: GNU/Linux',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    keywords='api rest authentication mongodb automatic web flask json',
     author='Felipe Valverde Campos',
     author_email='felipe.valverde.campos@gmail.com',
-    url='',
-    license='',
+    url='https://github.com/fvalverd/AutoApi',
+    license='MIT',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
@@ -23,6 +35,8 @@ setup(
         "pymongo==2.8",
         "pyOpenSSL==0.14"
     ],
+    tests_require=['nose'],
+    test_suite="tests",
     entry_points="""
     # -*- Entry points: -*-
     """,
