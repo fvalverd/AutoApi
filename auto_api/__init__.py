37e83f5071ad5694ea7d3536c9e9c9e32e5779cb
Felipe Valverde <felipe.valverde.campos@gmail.com>
2017-05-03 11:30:02 -0400

    refactor: path and methods on router

--- auto_api/__init__.py
+++ auto_api/__init__.py
@@ -15,9 +15,9 @@ class AutoApi(object):
         self.app = Flask(self.__class__.__name__)
         config(self.app)
         self.load_auth()
-        self.load_verbs()
+        self.load_methods()
 
-    def route(self, path, method, role=None, auth=True):
+    def route(self, auth=True, method='POST', path='/<api>/<path:path>', role=None):
         def wrapper(controller):
             return self.app.route(path, methods=[method])(
                 secure(self.app, role=role, auth=auth)(controller)
@@ -27,24 +27,18 @@ class AutoApi(object):
     def load_auth(self):
         # TODO: add invalid operation for /not_valid_op
         if self.auth:
-            self.route('/login', 'POST', auth=False)(login)
-            self.route('/logout', 'POST')(logout)
-            self.route('/user', 'POST', role='admin')(user)
-            self.route('/password', 'POST')(password)
-            self.route('/roles', 'POST', role='admin')(roles)
+            self.route(path='/login', auth=False)(login)
+            self.route(path='/logout')(logout)
+            self.route(path='/user', role='admin')(user)
+            self.route(path='/password')(password)
+            self.route(path='/roles', role='admin')(roles)
 
-    def load_verbs(self):
-        path = '/<api>/<path:path>'
-        self.route(path, 'GET', role='read')(get)
-        self.route(path, 'POST', role='create')(post)
-        self.route(path, 'DELETE', role='delete')(delete)
-        self.route(path, 'PUT', role='update')(put)
-        self.route(path, 'PATCH', role='update')(patch)
+    def load_methods(self):
+        self.route(method='GET', role='read')(get)
+        self.route(method='POST', role='create')(post)
+        self.route(method='DELETE', role='delete')(delete)
+        self.route(method='PUT', role='update')(put)
+        self.route(method='PATCH', role='update')(patch)
 
-    def run(host='0.0.0.0', port=8686, use_reloader=True, debug=True):
-        self.app.run(
-            host=host,
-            port=port,
-            use_reloader=use_reloader,
-            debug=debug
-        )
+    def run(self, host='0.0.0.0', port=8686, reloader=True, debug=True):
+        self.app.run(host=host, port=port, use_reloader=reloader, debug=debug)
