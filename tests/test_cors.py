c63e36a6808af7cc31b0ed65eedb987a53eef70d
Felipe Valverde <felipe.valverde.campos@gmail.com>
2017-06-17 13:50:10 -0400

    add: test cors headers

---
+++ tests/test_cors.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from auto_api import AutoApi
+from . import MONGO_PORT, MONGO_AUTH_PORT
+
+
+class AutoApiConfigTest(unittest.TestCase):
+
+    def _test_header(self, autoapi):
+        response = autoapi.app.test_client().get('/')
+        self.assertIn(
+            ('Access-Control-Allow-Origin', '*'),
+            response.headers.to_list()
+        )
+
+    def test_access_control_allow_origin_header(self):
+        self._test_header(AutoApi(port=MONGO_PORT))
+
+    def test_access_control_allow_origin_header_with_auth(self):
+        self._test_header(AutoApi(auth=True, port=MONGO_AUTH_PORT))
+
+
+if __name__ == '__main__':
+    unittest.main()
