cccf0ef4fc03018ff6555b4184a2e4b1fd7836ef
Felipe Valverde <felipe.valverde.campos@gmail.com>
2019-04-16 12:38:16 +0200

    fix: run_test

--- run_tests.py
+++ run_tests.py
@@ -10,97 +10,87 @@ from pymongo import MongoClient
 from mongobox import MongoBox
 
 
-MONGO_PORT, MONGO_AUTH_PORT = 27018, 27019
+SETTINGS_ENV = 'AUTOAPI_SETTINGS'
 AUTOAPI_SETTINGS_FILE = 'tests.cfg.default'
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+TMP_DIR = os.path.join(CURRENT_DIR, '.tmp')
+MONGO_PORT, MONGO_AUTH_PORT = 27018, 27019
+
+
+# set autoapi setting environment variable
+os.environ[SETTINGS_ENV] = os.path.join(CURRENT_DIR, AUTOAPI_SETTINGS_FILE)
+
+
+def _get_mongo_paths():
+    if not os.path.exists(TMP_DIR):
+        os.mkdir(TMP_DIR)
+    paths = os.path.join(TMP_DIR, 'data'), os.path.join(TMP_DIR, 'data_auth')
+    for path in paths:
+        if os.path.exists(path):
+            shutil.rmtree(path)
+    return paths
+
+
+def _get_config_module():
+    return imp.load_source('module.name', os.environ[SETTINGS_ENV])
+
+
+def _create_admin():
+    print(' - admin user on server auth...', end=' ')
+    sys.stdout.flush()
+    config = _get_config_module()
+    client = MongoClient(port=MONGO_AUTH_PORT)
+    client.admin.command(
+        'createUser',
+        config.MONGO_ADMIN,
+        pwd=config.MONGO_ADMIN_PASS,
+        roles=[
+            {'role': 'userAdminAnyDatabase', 'db': 'admin'},
+            'readWriteAnyDatabase'
+        ],
+        customData={'roles': ['admin']}
+    )
+    client.close()
+    print('OK')
 
 
 def run(args=None):
-    if args is None:
-        args = []
-
-    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-
-    # create temporal directory
-    TMP_DIR = os.path.join(CURRENT_DIR, '.tmp')
-    if not os.path.exists(TMP_DIR):
-        os.mkdir(TMP_DIR)
-
-    DB_PATH = os.path.join(TMP_DIR, 'data')
-    DB_PATH_AUTH = os.path.join(TMP_DIR, 'data_auth')
-
-    if os.path.exists(DB_PATH_AUTH):
-        shutil.rmtree(DB_PATH_AUTH)
-
-    # set autoapi environment variable for config
-    os.environ['AUTOAPI_SETTINGS'] = os.path.join(
-        CURRENT_DIR, AUTOAPI_SETTINGS_FILE
-    )
-
-    def create_admin():
-        print(' - admin user on server auth...', end=' ')
-        sys.stdout.flush()
-
-        # config module file
-        config = imp.load_source('module.name', os.environ['AUTOAPI_SETTINGS'])
-
-        client = MongoClient(port=MONGO_AUTH_PORT)
-        client.admin.command(
-            'createUser',
-            config.MONGO_ADMIN,
-            pwd=config.MONGO_ADMIN_PASS,
-            roles=[
-                {'role': 'userAdminAnyDatabase', 'db': 'admin'},
-                'readWriteAnyDatabase'
-            ],
-            customData={'roles': ['admin']}
-        )
-        client.close()
-        print('OK')
-
-    mongobox = MongoBox(mongod_bin='mongod', db_path=DB_PATH, port=MONGO_PORT)
-    mongoboxA = MongoBox(
-        mongod_bin='mongod', auth=True,
-        db_path=DB_PATH_AUTH, port=MONGO_AUTH_PORT
-    )
-
-    status, statusA = False, False
-    errno = 1
+    print('\nStarting mongo servers')
+    args = args or []
+    db_path, db_path_auth = _get_mongo_paths()
+    params = dict(mongod_bin='mongod', db_path=db_path, port=MONGO_PORT)
+    mongobox = MongoBox(**params)
+    params.update(dict(auth=True, db_path=db_path_auth, port=MONGO_AUTH_PORT))
+    mongoboxAuth = MongoBox(**params)
+    status = statusAuth = False
     try:
-        print('\nStarting mongo servers')
-
         # start server
-        print(' - server...', end=' ')
-        sys.stdout.flush()
+        print(' - server...', end=' '), sys.stdout.flush()
         mongobox.start()
         status = True
         print('OK')
 
         # start auth server
-        print(' - server auth...', end=' ')
-        sys.stdout.flush()
-        mongoboxA.start()
+        print(' - server auth...', end=' '), sys.stdout.flush()
+        mongoboxAuth.start()
         print('OK')
-        statusA = True
-        create_admin()
-        print('\n')
-        sys.stdout.flush()
+        statusAuth = True
+        _create_admin()
+        print('\n'), sys.stdout.flush()
 
-        errno = pytest.main(*args)
+        pytest.main(*args)
     finally:
         # stop servers
         if status:
             print('\n\nStoping mongo servers:')
-            print(' - server...', end=' ')
-            sys.stdout.flush()
+            print(' - server...', end=' '), sys.stdout.flush()
             mongobox.stop()
             print('OK')
-            if statusA:
-                print(' - server auth...', end=' ')
-                sys.stdout.flush()
-                mongoboxA.stop()
+            if statusAuth:
+                print(' - server auth...', end=' '), sys.stdout.flush()
+                mongoboxAuth.stop()
                 print('OK')
-    return errno
 
 
 if __name__ == '__main__':
-    sys.exit(run())
+    run()
